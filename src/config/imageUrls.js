6c6b49fa23fadb60d6b3980b3bf8f54af5cf8beb
0x-49 <maximeyao12345@gmail.com>
2025-02-16 17:17:13 +0000

    Update image handling to use S3 bucket URLs directly. Remove local image files and improve FeatureShowcase component

---
+++ src/config/imageUrls.js
@@ -0,0 +1,12 @@
+export const IMAGE_URLS = {
+  "1_": "https://landing-page-images-seo.s3.eu-north-1.amazonaws.com/1_.webp",
+  "2_": "https://landing-page-images-seo.s3.eu-north-1.amazonaws.com/2_.webp",
+  "3_": "https://landing-page-images-seo.s3.eu-north-1.amazonaws.com/3_.webp",
+  "4_": "https://landing-page-images-seo.s3.eu-north-1.amazonaws.com/4_.webp",
+  "5_": "https://landing-page-images-seo.s3.eu-north-1.amazonaws.com/5_.webp",
+  "6_": "https://landing-page-images-seo.s3.eu-north-1.amazonaws.com/6_.webp"
+};
+
+// Usage example:
+// import { IMAGE_URLS } from '../config/imageUrls';
+// const imageUrl = IMAGE_URLS['1_'];
